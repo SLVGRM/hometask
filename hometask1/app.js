0c5fe3614fe1e789cdcf4b69533166b75d74c62e
Larionov Viacheslav <lar.v@skbkontur.ru>
2024-04-02 15:23:28 +0500

    added fuction for calculation distance

---
+++ hometask1/app.js
@@ -0,0 +1,13 @@
+
+
+function GetDistance(posLatitude, posLongitude, destLatitude, destLongitude) {
+    // Проверка типов данных
+    if (typeof posLatitude !== 'number' || typeof posLongitude !== 'number' || typeof destLatitude !== 'number' || typeof destLongitude !== 'number') {
+        console.log("Ошибка: Все параметры должны быть числами.");
+        return;
+    }
+    let distance = Math.sqrt(Math.pow((Number(posLatitude) - destLatitude), 2) + Math.pow((posLongitude - destLongitude), 2));
+    console.log(distance);
+}
+
+GetDistance(1, 0, 3, 4)
